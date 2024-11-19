5547e6ee8cd6f690549446363c1e0d3f05a54a7b
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2024-11-19 01:41:42 -0600

    Add mailing functionality
    Users should be kept up to date on the status of their order with emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "depot.com",
+    authentication:       "plain",
+    user_name:            Rails.application.credentials.dig(:smtp, :user_name),
+    password:             Rails.application.credentials.dig(:smtp, :password),
+    enable_starttls_auto: true
+  }
+end
