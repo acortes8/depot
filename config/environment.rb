aabb3682ed3d84d1ab9474f5ccdd7946e0d310d2
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2024-11-20 12:37:01 -0600

    Configure for deployments on local host via tunnels
    Allows a web deployment which can help with testing on different devices

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ Rails.application.initialize!
 
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
