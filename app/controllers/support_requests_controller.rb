bd21369f7a382b6186c3bd6e6de5d1e74d292ee8
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2024-12-20 17:05:27 -0600

    Fix bug with trix
    Bug with importmaps not properly loading trix

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -3,6 +3,11 @@ class SupportRequestsController < ApplicationController
     @support_requests = SupportRequest.all
   end
 
+  def create
+    support_request = SupportRequest.create! params.expect(support_request: [:response])
+    redirect_to support_request
+  end
+
   def update
     support_request = SupportRequest.find(params[:id])
     support_request.update(response: params.require(:support_request)[:response])
